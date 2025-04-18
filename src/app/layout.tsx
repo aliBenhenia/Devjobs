b5edcba1d1ddf514976028cd517a0e6e06be8967
alibenhenia <alibenhenia1@gmail.com>
2025-04-18 20:15:14 +0100

    feat (style):  body class for dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
-      <body>
+      <body className="bg-bg dark:bg-bg-dark">
         {/* <ThemeToggle /> */}
         <NavbarLayout>{children}</NavbarLayout>
       </body>
