b43729980c681bb9026779c9337529a01e25d8da
aliBenhenia <alibenhenia1@gmail.com>
2025-04-22 12:25:42 +0200

    fix: update dark mode initialization logic for improved theme handling

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,21 +1,33 @@
 "use client";
 
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
-  const [darkMode, setDarkMode] = useState(() =>(typeof window !== 'undefined' && localStorage.theme === 'dark') || false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setDarkMode(storedTheme === "dark");
+    } else {
+      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      setDarkMode(prefersDark);
+    }
+  }, []);
 
   useEffect(() => {
     const root = window.document.documentElement;
     if (darkMode) {
-      root.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      root.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
+
   return (
     <div className="flex flex-col justify-center max-w-6xl mx-auto">
       <Header />
