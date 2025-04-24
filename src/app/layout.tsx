9bff649cfb543e6525481c6857c39b8b614ff581
aliBenhenia <alibenhenia1@gmail.com>
2025-04-24 18:29:33 +0200

    chore: mv global styles to styles folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
 import AppLayout from "@/components/layout/AppLayout";
 
 // import ThemeToggle from "./t";
