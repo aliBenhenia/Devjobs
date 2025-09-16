36a34fa6019a849bfbbed71c295d25b8ed7e13a9
Ali benhenia <alibenhenia1@gmail.com>
2025-09-16 20:59:36 +0000

    fix(button): remove onClick from <Button> to fix TypeScript type error

--- src/components/shared/search.tsx
+++ src/components/shared/search.tsx
@@ -167,7 +167,7 @@ export default function Search({ filters, onChange ,locations}: SearchProps) {
         {/* Search Button */}
         <div className="flex items-center justify-between px-6 py-4 w-full md:w-auto">
           <Button 
-            onClick={() => {}} 
+            // onClick={() => {}} 
             className="ml-0 md:ml-6 px-8 py-4 bg-accent hover:bg-accent/90 text-white font-bold rounded-lg transition-all duration-300 transform hover:scale-105 shadow-md hover:shadow-lg w-full md:w-auto flex items-center justify-center"
           >
             <SearchIcon className="w-5 h-5 mr-2" />
